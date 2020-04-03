98625c20a90b2b04126ec547e158698f96883d39
Jozy Kinnaman <jozypants@gmail.com>
2020-04-03 10:05:09 -0700

    add js functionality.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Business Logic
+function robotSpeak(userInput) {
+  var output = [];
+
+  for (var i = 0; i <= userInput; i++) {
+
+  }
+}
+
+// User interface logic
+$(document).ready(function () {
+  $("form#formOne").submit(function (event) {
+    event.preventDefault();
+    var userInput = parseInt($("input#userInput").val());
+  })
+})
